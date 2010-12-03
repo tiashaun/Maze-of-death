310f5ba082e760903e6c13dd310c568d013fd178
Jenso <jenso1988@gmail.com>
2010-12-03 13:56:57 +0100

    added some functions

--- Enemy.h
+++ Enemy.h
@@ -1,22 +1,34 @@
 /*
-* Enemy.h
-*
-* Created on: Nov 30, 2010
-* Author: matol004
-*/
+ * Enemy.h
+ *
+ *  Created on: Nov 30, 2010
+ *      Author: matol004
+ */
 
 #ifndef ENEMY_H_
 #define ENEMY_H_
 #include "Moving_Sprite.h"
 
-struct Nodes {
-int x,y;
+#include <vector>
+
+struct Node {
+	int x,y;
+};
+
+struct Coordinates{
+	int x, y;
 };
 
 class Enemy : public Moving_Sprite {
 public:
-Enemy(Level& level);
-virtual ~Enemy();
+	Enemy(Level& level);
+	virtual ~Enemy();
+	Node* get_target_node();
+	Coordinates calculate_direction(int object_x_coordinate, int object_y_coordinate,
+			int target_x_coordinate, int target_y_coordinate);
+private:
+	std::vector<Node*> nodes;
+	std::vector<Node*>::iterator current_target_node;
 };
 
 #endif /* ENEMY_H_ */
--- Enemy_Reactive.h
+++ Enemy_Reactive.h
@@ -31,10 +31,10 @@ public:
 	void move();
 	void update();
 private:
+	Player& reference_to_player;		// The player object on the level.
 	Gamerules& gamerules;
 	bool following_player;				// Is the enemy following the player
 	uint followed_for;					// How long the enemy should follow the player after the player has left the attack_area_circle.
-	Player& reference_to_player;		// The player object on the level.
 	Circle attack_area_circle;			// The enemys detection radius, centered around the enemys image on the map.
 
 	void update_circle(int x, int y);
